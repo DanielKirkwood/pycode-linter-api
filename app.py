f40fca806eaef2e51eedacf6917ba10f1d351172
Daniel Kirkwood <2324926k@student.gla.ac.uk>
2021-12-21 22:09:08 +0000

    Update to return JSON with formated messages

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import uuid
 import os
 from flask import Flask, request
-from pylint import epylint as lint
+import pylint.lint
+import pprint
 
 app = Flask(__name__)
 
@@ -15,21 +16,39 @@ def linter():
     user_code = json['code']
 
     # create temp file to contain users code
-    temp_name = uuid.uuid4()
-    temp_file = f'{temp_name}.py'
+    temp_name = uuid.uuid4().hex
+    temp_file = temp_name + '.py'
     with open(temp_file, 'w') as file:
       file.write(str(user_code))
 
-    # run pylint on file and capture output
-    (pylint_stdout, pylint_stderr) = lint.py_run(temp_file, return_std=True)
+    options = [
+      temp_file,
+      '--output-format=json'
+    ]
+    results = pylint.lint.Run(options, do_exit=False).linter.reporter.messages
 
     # delete temp file as no longer needed
     if os.path.exists(temp_file):
-      os.remove(temp_file)
+      try:
+        os.remove(temp_file)
+      except OSError as e:
+        print("Failed with:", e.strerror)
+        print("Error code:", e.code)
 
-    return pylint_stdout.getvalue()
+
+    messages = []
+    for m in results:
+      messages.append({
+        "messageID": m[0],
+        "symbol": m[1],
+        "message": m[2],
+        "type": m[4],
+        "line": m[10]
+      })
+
+    return {"payload": {"result": messages}, "success": "true"}
   else:
-    return 'Content-Type not supported!'
+    return {"payload": {"result": 'Content-Type not supported!'}, "success": "false"}
 
 if __name__ == "__main__":
   app.run()
